abcafecfa9e21e05d39d558527a2f0fc5e4094d7
Alex Ellis <alexellis2@gmail.com>
2016-04-08 23:05:27 +0100

    Dockerfile for node v1

---
+++ node_v1/build_docker.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+docker build -t xservedbyfinder:node_v1 .
+
+echo "Built as: xservedbyfinder:node_v1"
---
+++ node_v1/run_docker.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "Control+C when done"
+
+docker run -t xservedbyfinder:node_v1
+
