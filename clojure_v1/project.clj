f622f048406a096209cd26597886fa22c935c540
Tom Cooper <tom.cooper@adp.com>
2016-04-12 16:21:16 -0400

    update project file

--- clojure_v1/project.clj
+++ clojure_v1/project.clj
@@ -1,8 +1,7 @@
 (defproject xservedbyfinder "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "xservedbyfinder in clojure"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "2.0.0"]]
   :main ^:skip-aot xservedbyfinder.core
