cd1689d66223bd7e12b51965d9660e8ad135898a
davehouse <dave.a.house@gmail.com>
2016-05-01 06:43:48 -0600

    use port. move todo's

--- bash_v3/find.sh
+++ bash_v3/find.sh
@@ -9,8 +9,7 @@ if [ ${BASH_VERSINFO[0]} -lt 4 ]; then
 fi
 
 declare -A dict
-# If http
-exec 3<>/dev/tcp/${host}/80
+exec 3<>/dev/tcp/${host}/${port}
 echo -e "GET / HTTP/1.1\r\nConnection: close\r\n\r\n" >&3
 proto=https
 while IFS='' read -r line; do
@@ -18,10 +17,8 @@ while IFS='' read -r line; do
     if [[ ${#line} -gt 12 && ${line:0:12} = 'X-Served-By:' ]]; then
         echo "http"
         proto=http
-        # trim start instead since we know it starts with X-Served...
         key=$(expr match "$line" 'X-Served-By: \(.\+\)')
         key="${key//[[:space:]]/ }"
-        # save file as name instead. overwrites if existing. slow?
         dict[$key]=1
     fi
 done <&3
@@ -35,9 +32,11 @@ if [ $proto = 'http' ];then
         echo -e "GET / HTTP/1.1\r\nConnection: close\r\n\r\n" >&3
         while IFS='' read -r line; do
             echo $line
+            # TODO: trim start instead since we know it starts with X-Served...
             if [[ ${#line} -gt 12 && ${line:0:12} = 'X-Served-By:' ]]; then
                 key=$(expr match "$line" 'X-Served-By: \(.\+\)')
                 key="${key//[[:space:]]/ }"
+                # TODO: save file as name instead. overwrites if existing. slow?
                 dict[$key]=1
                 break
             fi
