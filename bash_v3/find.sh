ae362d55c0a11fbe339b37881c737ff9a537488d
davehouse <dave.a.house@gmail.com>
2016-05-01 07:18:48 -0600

    loop in same ssl session

--- bash_v3/find.sh
+++ bash_v3/find.sh
@@ -8,12 +8,12 @@ if [ ${BASH_VERSINFO[0]} -lt 4 ]; then
     exit
 fi
 
+req="HEAD / HTTP/1.1\r\nHost: ${host}\r\nConnection: close\r\n\r\n"
 declare -A dict
 exec 3<>/dev/tcp/${host}/${port}
-echo -e "GET / HTTP/1.1\r\nConnection: close\r\n\r\n" >&3
+echo -e "${req}" >&3
 proto=https
 while IFS='' read -r line; do
-    echo $line
     if [[ ${#line} -gt 12 && ${line:0:12} = 'X-Served-By:' ]]; then
         echo "http"
         proto=http
@@ -29,7 +29,7 @@ if [ $proto = 'http' ];then
     echo "http"
     for i in {0..10}; do
         exec 3<>/dev/tcp/${host}/${port}
-        echo -e "GET / HTTP/1.1\r\nConnection: close\r\n\r\n" >&3
+        echo -e "${req}" >&3
         while IFS='' read -r line; do
             echo $line
             # TODO: trim start instead since we know it starts with X-Served...
@@ -43,18 +43,19 @@ if [ $proto = 'http' ];then
         done <&3
     done
 else
-    echo -e "HEAD / HTTP/1.1\r\nHost: ${host}\r\nConnection: close\r\n\r\n" | 
-    exec 3<>openssl s_client -ign_eof -connect ${host}:443 -CAfile /etc/ssl/certs/ca-certificates.crt 2> /dev/null
-    echo -e "GET / HTTP/1.1\r\nConnection: close\r\n\r\n" >&3
+    echo "https"
+    req="HEAD / HTTP/1.1\r\nHost: ${host}\r\n"
+    close="${req}Connection: close\r\n\r\n"
     while IFS='' read -r line; do
-        echo $line
+        # TODO: trim start instead since we know it starts with X-Served...
         if [[ ${#line} -gt 12 && ${line:0:12} = 'X-Served-By:' ]]; then
             key=$(expr match "$line" 'X-Served-By: \(.\+\)')
             key="${key//[[:space:]]/ }"
+            # TODO: save file as name instead. overwrites if existing. slow?
             dict[$key]=1
-            break
+            continue
         fi
-    done <&3
+    done < <((for i in {0..10};do echo -e "${req}\r\n";done;echo -e "${close}")|openssl s_client -ign_eof -connect ${host}:443 -CAfile /etc/ssl/certs/ca-certificates.crt 2> /dev/null)
 fi
 
 for K in "${!dict[@]}"; do
