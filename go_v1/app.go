d4a7219f78b40cb447f8516e68617272d786256f
Alex <alexellis2@gmail.com>
2016-09-19 22:34:35 +0100

    Reformat imports

--- go_v1/app.go
+++ go_v1/app.go
@@ -1,55 +1,56 @@
 package main
 
-import "net/http"
-import "fmt"
-import "os"
-import "strconv"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+)
 
 func getservedby(url string) (header string) {
-    resp, err := http.Get(url)
-    header = ""
-    if err != nil {
-      fmt.Printf("-")
+	resp, err := http.Get(url)
+	header = ""
+	if err != nil {
+		fmt.Printf("-")
 
-    } else {
-      defer resp.Body.Close()
-
-      if(resp.Status == "200 OK") {
-          fmt.Printf(".")
-          header = resp.Header.Get("X-served-by")
-      } else {
-          fmt.Printf("-")
-      }
-    }
-    return
+	} else {
+		defer resp.Body.Close()
+		if resp.Status == "200 OK" {
+			fmt.Printf(".")
+			header = resp.Header.Get("X-served-by")
+		} else {
+			fmt.Printf("-")
+		}
+	}
+	return
 }
 
 func main() {
 
-    reqs := 10
-    args := os.Args[1:]
-    if(len(args) != 0) {
-        amt, _ := strconv.Atoi(args[0])
-        reqs = amt
-    }
-    fmt.Printf("Reqs: %v\n", reqs)
+	reqs := 10
+	args := os.Args[1:]
+	if len(args) != 0 {
+		amt, _ := strconv.Atoi(args[0])
+		reqs = amt
+	}
+	fmt.Printf("Reqs: %v\n", reqs)
 
-    url := "http://localhost:3000/"
-    if len(os.Getenv("URL")) >0 {
-        url = os.Getenv("URL")
-    }
-    fmt.Printf("Using url: %v", url)
-    found := make(map[string]int)
-    for i:=0; i< reqs; i++ {
-        code := getservedby(url)
-        if _, exists := found[code]; !exists {
-            found[code]=0
-        }
-        found[code] = found[code] + 1
-    }
-    fmt.Printf("\n")
-    for k,_ := range found {
-        fmt.Printf("[%v]\t%v\n", k, found[k]);
-    }
+	url := "http://localhost:3000/"
+	if len(os.Getenv("URL")) > 0 {
+		url = os.Getenv("URL")
+	}
 
+	fmt.Printf("Using url: %v", url)
+	found := make(map[string]int)
+	for i := 0; i < reqs; i++ {
+		code := getservedby(url)
+		if _, exists := found[code]; !exists {
+			found[code] = 0
+		}
+		found[code] = found[code] + 1
+	}
+	fmt.Printf("\n")
+	for k, _ := range found {
+		fmt.Printf("[%v]\t%v\n", k, found[k])
+	}
 }
